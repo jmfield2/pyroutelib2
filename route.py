ca6488fc963b94dced5a4d65c2cf7bf29286d398
jmfield2 <jmfield2@mail.usf.edu>
2015-05-13 23:37:38 -0400

    Routing fixes and usability enhancements

--- route.py
+++ route.py
@@ -60,36 +60,60 @@ class Router:
 
     try:
       for i, weight in self.data.routing[start].items():
+        n=self.data.findNode(float(self.data.rnodes[i][0]), float(self.data.rnodes[i][1]))
         self.addToQueue(start,i, blankQueueItem, weight)
     except KeyError:
       return('no_such_node',[])
 
+    # Should only find local nodes to search instead of the entire graph XXX
+   
     # Limit for how long it will search
     count = 0
-    while count < 1000000:
+    success = []
+    while count < 1000:
       count = count + 1
       try:
         nextItem = self.queue.pop(0)
       except IndexError:
         # Queue is empty: failed
-        # TODO: return partial route?
-        return('no_route',[])
+	if len(success) > 0: break # we could have found one path previously...
+	#else: TODO: return partial route?
+        else: return('no_route',[])
       x = nextItem['end']
       if x in closed:
         continue
       if x == end:
         # Found the end node - success
         routeNodes = [int(i) for i in nextItem['nodes'].split(",")]
-        return('success', routeNodes)
-      closed.append(x)
+        success.append( [routeNodes, nextItem] ) #return('success', [routeNodes, nextItem] )
+        continue
+      else: closed.append(x)
       try:
         for i, weight in self.data.routing[x].items():
           if not i in closed:
             self.addToQueue(x,i,nextItem, weight)
-      except KeyError:
+     
+      except KeyError as e:
         pass
-    else:
-      return('gave_up',[])
+    
+    dist = [sys.maxint, 0]
+    for x in success:
+      if x[1]['distance'] < dist[0]: dist = [x[1]['distance'], x]
+
+    #print success
+ 
+    if len(success) <= 0: 
+	for q in self.queue:
+		n = []
+		nodes = q['nodes'].split(',')
+		for x in nodes:
+			n.append( (float(self.data.rnodes[x][0]), float(self.data.rnodes[x][1])) )
+
+                from polyline.codec import PolylineCodec
+		print PolylineCodec().encode(n)
+	
+	return ('gave_up', [])
+    else: return ('success', dist[1])
   
   def addToQueue(self,start,end, queueSoFar, weight = 1):
     """Add another potential route to the queue"""
@@ -99,6 +123,7 @@ class Router:
     #
     # TODO: we could reduce downloads even more by only getting data around
     # the tip of the route, rather than around all nodes linked from the tip
+    if end not in self.data.rnodes: return
     end_pos = self.data.rnodes[end]
     self.data.getArea(end_pos[0], end_pos[1])
     
@@ -108,9 +133,9 @@ class Router:
         return
     distance = self.distance(start, end)
     if(weight == 0):
-      return
+      pass#return
     distance = distance / weight
-    
+
     # Create a hash for all the route's attributes
     distanceSoFar = queueSoFar['distance']
     queueItem = { \
